339f326346518d8e6ff661c3611a2ad1c1e6d625
shivamvora <shivampatel27102001@gmail.com>
2022-03-19 21:34:29 +0530

    push final code

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 
 const Form = () => {
@@ -17,12 +18,45 @@ const Form = () => {
         value = e.target.value;
         setField( { ...field, [name]: value } )
     }
+
     if ( field.name !== '', field.lastname !== '', field.email !== '', field.message !== '' ) {
         console.log( 'what is filed now', field );
     }
+
+    const formSubmit = ( e ) => {
+        e.preventDefault();
+        let data = {
+            name: field.name,
+            email: field.email,
+            message: field.message,
+            lastname: field.lastname,
+        }
+        axios.post( '/api/form', data )
+            .then( res => {
+                console.log( `res is following ${res}` );
+                setField( { sent: true } )
+            }, resetForm() ).catch( () => {
+                console.log( `message not sent` );
+            } )
+    }
+
+    const resetForm = () => {
+        setField( {
+            name: '',
+            lastname: '',
+            email: '',
+            message: ''
+        } )
+        setTimeout( () => {
+            setField( {
+                sent: false
+            } )
+        }, 3000 );
+    }
+
     return (
         <div className="container">
-            <form>
+            <form onSubmit={formSubmit}>
                 <div className="form-group">
                     <label htmlFor='name'>Name</label>
                     <input type="text" name='name' className="form-control name" placeholder="your name..." onChange={handleInputs} value={field.name} />
@@ -44,9 +78,11 @@ const Form = () => {
                     <textarea name='message' className="form-control message" placeholder="your message..." onChange={handleInputs} value={field.message} />
                 </div>
 
-                <div className="alert alert-success" role="alert">
-                    Message has been sent...!
-                </div>
+                {field.sent === true ?
+                    <div className="alert alert-success" role="alert">
+                        Message has been sent...!
+                    </div>
+                    : ''}
 
                 <button type='submit' className='btn btn-outline-primary'>submit</button>
             </form >
